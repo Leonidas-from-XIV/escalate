3c6e3daf8f970b250e88303d1b75e7a89068be96
Marek Kubica <marek@xivilization.net>
2010-01-02 23:46:58 +0100

    Added some types that can be used to contain the data

--- inflate.ml
+++ inflate.ml
@@ -1,2 +1,9 @@
+type blockType = Uncompressed | FixedHuffman | DynamicHuffman | Reserved
+type blockFinal = Continues | Last
+type compressedContent = Payload of string
+type deflateBlock = ( blockFinal * blockType * compressedContent )
+
+let parse bytestring = [ (Last, Uncompressed, Payload "foo") ]
+
 let inflate = function
         | _ -> "Foo"
