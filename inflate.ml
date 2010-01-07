c4cfa110763f3aebcab98501f24af06c4776af4e
Marek Kubica <marek@xivilization.net>
2010-01-07 17:06:29 +0100

    Added some (broken) types for the zlib wrapper

--- inflate.ml
+++ inflate.ml
@@ -1,7 +1,20 @@
+(* deflate *)
 type blockType = Uncompressed | FixedHuffman | DynamicHuffman | Reserved
 type blockFinal = Continues | Last
-type compressedContent = Payload of string
-type deflateBlock = ( blockFinal * blockType * compressedContent )
+type deflatedContent = Payload of string
+type deflateBlock = ( blockFinal * blockType * deflatedContent )
+
+(* zlib *)
+type compressionMethod = Deflate | NotDeflate
+type compressionInfo = WindowSize of int
+type dictPresent = bool
+type presetDictionary = Dictionary
+type compressionLevel = FastestCompression | FastCompression | DefaultCompression | MaximalCompression
+type checksumHeader = int
+type compressedContent = list deflateBlock
+type checksumData = Adler32 of int
+type zlibBlock = ( compressionMethod * compressionInfo * checksumHeader *
+dictPresent * compressionLevel * presetDictionary * compressedContent * checksumData )
 
 let parse bytestring = [ (Last, Uncompressed, Payload "foo") ]
 
