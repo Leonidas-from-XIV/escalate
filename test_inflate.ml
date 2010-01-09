b760f65dee4939c10c49f88974bd6cd6ff30d578
Marek Kubica <marek@xivilization.net>
2010-01-09 19:47:51 +0100

    Refactored tests a bit, added beginnings of a header test

--- test_inflate.ml
+++ test_inflate.ml
@@ -1,9 +1,15 @@
 open OUnit
 open Inflate
 
+let zlib_compressed = "x\x9cs\xcaI\xccPH\xc2$\x14\x01o\x19\x08u"
+let plaintext = "Blah blah blah blah blah!"
+
+let read_cm_info () =
+        let cm = parse_header zlib_compressed in
+        assert_equal cm 6
+
 let inflate_blah () =
-        assert_equal (inflate "x\x9cs\xcaI\xccPH\xc2$\x14\x01o\x19\x08u")
-        "Blah blah blah blah blah!"
+        assert_equal (inflate zlib_compressed) plaintext
 
 let suite = "Inflate" >::: [
         "inflate_blah" >:: inflate_blah
