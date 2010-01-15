d64f374a469a7b9a0c7d8bc88bb245a6d34d32bb
Marek Kubica <marek@xivilization.net>
2010-01-15 09:00:38 +0100

    Split large unittest into smaller indenpendently failing parts

--- test_inflate.ml
+++ test_inflate.ml
@@ -4,15 +4,11 @@ open Inflate
 let zlib_compressed = "x\x9cs\xcaI\xccPH\xc2$\x14\x01o\x19\x08u"
 let plaintext = "Blah blah blah blah blah!"
 
-let read_zlib_header () =
+let read_zlib_header expected function_got () =
         let printer = Some string_of_int in
         let header = parse_zlib_header zlib_compressed in
         let assert_header = assert_equal ?printer:printer in
-        assert_header 8 header.cm;
-        assert_header 7 header.cinfo;
-        assert_header 2 header.flevel;
-        assert_header 28 header.fcheck;
-        assert_header 30876 header.checksum
+        assert_header expected (function_got header)
 
 let inflate_blah () =
         let printer = Some (fun x -> x) in
@@ -20,7 +16,12 @@ let inflate_blah () =
 
 let suite = "Inflate" >::: [
         "inflate_blah" >:: inflate_blah;
-        "read_zlib_header" >:: read_zlib_header
+        "read_zlib_header_cm" >:: read_zlib_header 8 (fun h -> h.cm);
+        "read_zlib_header_cinfo" >:: read_zlib_header 7 (fun h -> h.cinfo);
+        "read_zlib_header_flevel" >:: read_zlib_header 2 (fun h -> h.flevel);
+        "read_zlib_header_fcheck" >:: read_zlib_header 28 (fun h -> h.fcheck);
+        "read_zlib_header_checksum" >:: read_zlib_header 30876 (fun h ->
+                h.checksum)
 ]
 
 let _ = run_test_tt_main suite
